ea87f51fac7464c0ab25cd0bc08d9f49539427a2
Cosban <buckley@phobiasystems.net>
2014-08-31 14:30:43 -0600

    gotta update the resources

--- plugin.rb
+++ plugin.rb
@@ -3,5 +3,5 @@
 # version: 0.1
 # authors: cosban and fysx
 
-register_asset "javascripts/raptorize/jquery-1.4.1.min.js", :client_side
+register_asset "javascripts/raptorize/modernizr.audio.js", :client_side
 register_asset "javascripts/raptorize/jquery.raptorize.1.0.js", :client_side
